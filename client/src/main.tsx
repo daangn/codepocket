a819dd5ca6c1daaad4da7e1e2e89c24fac2fee84
junghyeonsu <jung660317@naver.com>
2022-08-01 15:51:40 +0900

    feat(client): Auth0Provider 생성

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,7 @@
 import './shared/styles/global.css';
 
+import { Auth0Provider } from '@auth0/auth0-react';
+import { AUTH0_CLIENT_ID, AUTH0_DOMAIN } from '@shared/constant';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import React from 'react';
@@ -14,11 +16,16 @@ const isDev = process.env.NODE_ENV === 'development';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <App />
-        {isDev && <ReactQueryDevtools initialIsOpen={false} />}
-      </QueryClientProvider>
+      <Auth0Provider
+        domain={AUTH0_DOMAIN}
+        clientId={AUTH0_CLIENT_ID}
+        redirectUri={window.location.origin}
+      >
+        <QueryClientProvider client={queryClient}>
+          <App />
+          {isDev && <ReactQueryDevtools initialIsOpen={false} />}
+        </QueryClientProvider>
+      </Auth0Provider>
     </BrowserRouter>
-    ,
   </React.StrictMode>,
 );
--- client/src/shared/constant.ts
+++ client/src/shared/constant.ts
@@ -1,2 +1,5 @@
 export const BASE_LOCAL_SERVER_URL = import.meta.env.VITE_BASE_SERVER_URL_DEV;
 export const BASE_PROD_SERVER_URL = import.meta.env.VITE_BASE_SERVER_URL_PROD;
+
+export const AUTH0_DOMAIN = import.meta.env.VITE_AUTH0_DOMAIN;
+export const AUTH0_CLIENT_ID = import.meta.env.VITE_AUTH0_CLIENT_ID;
