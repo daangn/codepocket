d038dcf0f68cafed76d239bad5ff889cebc1d2ec
shellboy <qkrdmstlr3@naver.com>
2022-08-01 17:15:55 +0900

    feat(server): createUser api 구현

---
+++ server/src/connectDB/createUser.ts
@@ -0,0 +1,31 @@
+import { CreateUserRequest, CreateUserResponse } from '@pocket/schema';
+import to from 'await-to-js';
+import { FastifyInstance, FastifyRequest } from 'fastify';
+import stringHash from 'string-hash';
+
+import { CustomResponse } from '../utils/responseHandler';
+
+export default async (server: FastifyInstance, request: FastifyRequest) => {
+  const {
+    body: { userName, email },
+  } = request as CreateUserRequest;
+
+  if (!userName || email) throw new CustomResponse({ customStatus: 4000 });
+
+  const [error, isExistUser] = await to(
+    (async () => await server.store.User.findOne({ userName, email }))(),
+  );
+  if (error) throw new CustomResponse({ customStatus: 5000 });
+  if (isExistUser) throw new CustomResponse({ customStatus: 5000 });
+
+  const token = String(stringHash(userName));
+  const [createError] = await to(
+    (async () => await server.store.User.create({ userName, email, token }))(),
+  );
+  if (createError) throw new CustomResponse({ customStatus: 5000 });
+
+  return new CustomResponse<CreateUserResponse>({
+    customStatus: 2002,
+    body: { pocketToken: token },
+  });
+};
