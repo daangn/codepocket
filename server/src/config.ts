f8a7607c39f9b24ac690621af95b8bf8e20e303c
junghyeonsu <jung660317@naver.com>
2022-08-02 19:28:24 +0900

    chore(server,cli): dotenv -> dotenv-safe로 변경

--- server/src/config.ts
+++ server/src/config.ts
@@ -1,7 +1,10 @@
-import dotenv from 'dotenv';
+import dotenv from 'dotenv-safe';
 import path from 'path';
 
 (() => {
-  const result = dotenv.config({ path: path.join(__dirname, '../..', '.env') });
+  const result = dotenv.config({
+    path: path.join(__dirname, '../..', '.env'),
+    example: path.join(__dirname, '../..', '.env.example'),
+  });
   if (!result.parsed) throw new Error('.env 환경변수 파일 읽기를 실패했어요');
 })();
