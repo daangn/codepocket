d74a889bfaf04c25d132a7f2a5f26f7404976ab0
junghyeonsu <jung660317@naver.com>
2022-08-02 20:16:26 +0900

    chore: CI 변경

--- cli/lib/config.ts
+++ cli/lib/config.ts
@@ -5,7 +5,7 @@ import path from 'path';
   const result = dotenv.config({
     path: path.join(__dirname, '../..', '.env'),
     example: path.join(__dirname, '../..', '.env.example'),
-    allowEmptyValues: !!process.env.GITHUB_ACTION_CI,
+    allowEmptyValues: !!process.env.CI,
   });
   if (!result.parsed) throw new Error('.env 환경변수 파일 읽기를 실패했어요');
 })();
