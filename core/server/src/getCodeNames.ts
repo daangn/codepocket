2f92b431d88e82e675ca01b78682614e919212b1
shellboy <qkrdmstlr3@naver.com>
2022-08-07 14:22:09 +0900

    refactor(schema): getServerLogin schema 변경

--- core/server/src/getCodeNames.ts
+++ core/server/src/getCodeNames.ts
@@ -14,8 +14,8 @@ export default async <T, Response>(request: T, modules: GetCodeNames<Response>) 
   if (!getCodeNamesRequestValidate(request)) throw modules.validateErrorFunc();
   const { codeAuthor, codeName } = request.query;
 
-  const codeNameRegex = new RegExp(codeName, 'gi');
-  const codeAuthorRegex = new RegExp(codeAuthor, 'gi');
+  const codeNameRegex = new RegExp(codeName || '', 'gi');
+  const codeAuthorRegex = new RegExp(codeAuthor || '', 'gi');
 
   const { codeAuthors, codeNames } = await modules.findCodeInfoUsingRegex({
     codeAuthorRegex,
