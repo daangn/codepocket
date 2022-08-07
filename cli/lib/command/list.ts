afbc1454f1bc0dbce51c21382193f52ac231bec7
shellboy <qkrdmstlr3@naver.com>
2022-08-07 14:22:49 +0900

    refactor(cli): getCodeNames 인자 변경

--- cli/lib/command/list.ts
+++ cli/lib/command/list.ts
@@ -4,19 +4,19 @@ import chalk from 'chalk';
 import { listCodeAPI } from '../api';
 import { checkaServerEnv, logger } from '../utils';
 
-export default async ({ codeName, codeAuthor }: { codeName?: string; codeAuthor?: string }) => {
+export default async ({ fileName, author }: { fileName?: string; author?: string }) => {
   const [error, result] = await to(
     (async () => {
       checkaServerEnv();
       const response = await listCodeAPI({
-        codeName: codeName || '',
-        codeAuthor: codeAuthor || '',
+        codeName: fileName || '',
+        codeAuthor: author || '',
       });
       return response;
     })(),
   );
   if (error) return logger.error(chalk.yellow(error.message));
-  const names = result.fileNames.join('\n');
-  const authors = result.authors.join(', ');
+  const names = result.codeNames.join('\n');
+  const authors = [...new Set(result.authors)].join(', ');
   return logger.info(`${chalk.green(authors || '모두')}의 코드들입니다🥕\n${names}`);
 };
