6206522f5a5b2d7d79cc374e1c815f2a678bd467
shellboy <qkrdmstlr3@naver.com>
2022-08-07 14:52:16 +0900

    refactor(client): clientstory 다시 살림

--- client/src/detail/hooks/useCreateStory.ts
+++ client/src/detail/hooks/useCreateStory.ts
@@ -3,10 +3,12 @@ import {
   CreateStoryResponse,
   createStoryResponseValidate,
 } from '@pocket/schema';
+import { EMAIL_DOMAIN_NAME } from '@shared/constant';
 import useCustomMutation from '@shared/hooks/useCustomMutation';
 import { localStorage } from '@shared/utils/localStorage';
 import { useQueryClient } from '@tanstack/react-query';
 
+import useCustomAuth0 from '../../auth/hooks/useCustomAuth0';
 import { createStoryUrl, getStoryNamesUrl, StoryFullName } from '../api';
 
 type CreateStoryBodyType = CreateStoryRequest['body'];
@@ -18,6 +20,7 @@ interface UseCreateStory {
 
 const useCreateStory = ({ codeAuthor, codeName, selectStory }: UseCreateStory) => {
   const queryClient = useQueryClient();
+  const { user } = useCustomAuth0({ domain: EMAIL_DOMAIN_NAME });
   const { mutate: createStoryMutate } = useCustomMutation<
     CreateStoryResponse,
     CreateStoryResponse,
@@ -28,27 +31,18 @@ const useCreateStory = ({ codeAuthor, codeName, selectStory }: UseCreateStory) =
     validator: createStoryResponseValidate,
     options: {
       onSuccess: async (_, vars) => {
+        if (!user) return;
         await queryClient.invalidateQueries([getStoryNamesUrl]);
-        // TODO: 로컬스토리지에서 username받아오는게 token으로 바껴서 로직 수정 필요
-        const storyAuth = localStorage.getUserToken();
-        const storyName = `${storyAuth}-${vars.storyName}` as StoryFullName;
+        const storyName = `${user.nickname}-${vars.storyName}` as StoryFullName;
         selectStory(`${codeAuthor}/${codeName}_${storyName}`);
       },
     },
   });
 
   const createStory = ({ codes, storyName }: Pick<CreateStoryBodyType, 'codes' | 'storyName'>) => {
-    // TODO: 로컬스토리지에서 username받아오는게 token으로 바껴서 로직 수정 필요
-    const storyAuthor = localStorage.getUserToken();
-    if (!storyAuthor || !storyName || !codeName || !codeAuthor) return;
-
-    createStoryMutate({
-      codes,
-      storyName,
-      codeName,
-      codeAuthor,
-      pocketToken: storyAuthor,
-    });
+    const pocketToken = localStorage.getUserToken();
+    if (!pocketToken || !storyName || !codeName || !codeAuthor) return;
+    createStoryMutate({ codes, storyName, codeName, codeAuthor, pocketToken });
   };
 
   return {
