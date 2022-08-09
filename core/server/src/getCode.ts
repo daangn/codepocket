c2d902f909b5ad37b7be11430357e89d527edb2d
shellboy <qkrdmstlr3@naver.com>
2022-08-09 11:16:38 +0900

    feat(core/server): 서버 코어로직 구현

---
+++ core/server/src/getCode.ts
@@ -0,0 +1,18 @@
+import { getCodeRequestValidate, GetCodeResponse } from '@pocket/schema';
+import { CodeId, CodeInfoWithCode } from 'types';
+
+interface GetCodeType<Response> {
+  validateErrorFunc: () => Response;
+  successResponseFunc: (body: GetCodeResponse) => Response;
+  getCodeById: (params: CodeId) => Promise<CodeInfoWithCode>;
+}
+
+export default async <T, Response>(request: T, modules: GetCodeType<Response>) => {
+  if (!getCodeRequestValidate(request)) throw modules.validateErrorFunc();
+  const { codeId } = request.query;
+
+  const codeInfo = await modules.getCodeById({ codeId });
+
+  const { code, codeAuthor, codeName, isAnonymous } = codeInfo;
+  return modules.successResponseFunc({ code, codeAuthor, codeName, isAnonymous, message: '' });
+};
