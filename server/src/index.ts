9330128901db98cc30a95b5f5ce0ae7c7b980868
shellboy <qkrdmstlr3@naver.com>
2022-08-10 18:44:31 +0900

    .

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,11 +4,11 @@ import cors from '@fastify/cors';
 import { to } from 'await-to-js';
 import fastify, { FastifyInstance as H } from 'fastify';
 import { IncomingMessage, Server, ServerResponse } from 'http';
-import mongoose from 'mongoose';
 
+// import mongoose from 'mongoose';
 import route from './router';
-import { CodeSchema, StorySchema, UserSchema } from './schema';
-import { env } from './utils/env';
+// import { CodeSchema, StorySchema, UserSchema } from './schema';
+// import { env } from './utils/env';
 
 const PORT = Number(process.env.PORT) || 8080;
 const server: H<Server, IncomingMessage, ServerResponse> = fastify({ logger: true });
@@ -20,18 +20,18 @@ server.register(cors, () => {
   };
 });
 server.register(route);
-server.register(async () => {
-  const [err, connection] = await to(
-    mongoose.connect(env.MONGO_DB_URI || '', { dbName: env.MONGO_DB_NAME }),
-  );
-  if (err) throw err;
-  server.decorate('store', {
-    User: connection.model('User', UserSchema),
-    Code: connection.model('Code', CodeSchema),
-    Story: connection.model('Story', StorySchema),
-    db: connection,
-  });
-});
+// server.register(async () => {
+//   const [err, connection] = await to(
+//     mongoose.connect(env.MONGO_DB_URI || '', { dbName: env.MONGO_DB_NAME }),
+//   );
+//   if (err) throw err;
+//   server.decorate('store', {
+//     User: connection.model('User', UserSchema),
+//     Code: connection.model('Code', CodeSchema),
+//     Story: connection.model('Story', StorySchema),
+//     db: connection,
+//   });
+// });
 
 const start = async () => {
   const [err] = await to(server.listen({ port: PORT, host: '0.0.0.0' }));
