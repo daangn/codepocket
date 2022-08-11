545706a683081c08a92da10ebcf73e39f5680fa7
junghyeonsu <jung660317@naver.com>
2022-08-11 19:24:47 +0900

    chore: 타이틀 제거

--- client/src/token/Token.tsx
+++ client/src/token/Token.tsx
@@ -20,7 +20,6 @@ function TokenPage() {
         토큰이 발급되었어요.
       </h1>
       <Alert status="error">
-        <Alert.Title>타이틀이에요</Alert.Title>
         <Alert.Description>
           cli에 사용되는 토큰이에요. 아래 토큰을 터미널에 등록해야해요{`\n`}
           해당 토큰은 이 페이지를 나가면 알 수 없어요.
