18c1759ee6bc721ee2e9b95f1b3dcb87a2dd55f3
junghyeonsu <jung660317@naver.com>
2022-08-12 17:31:38 +0900

    chore: AuthPage 로직 변경

--- client/src/auth/Auth.tsx
+++ client/src/auth/Auth.tsx
@@ -35,10 +35,7 @@ const AuthPage: React.FC = () => {
       return;
     }
 
-    const email = !isExternalUser ? user.email : '';
-    if (!email) return;
-
-    createUser({ userName: user.nickname, email });
+    createUser({ userName: user.nickname, email: user.email || '' });
   }, [user, isValidEmailDomain, isAuthenticated, logout, navigate, createUser, isExternalUser]);
 
   return (
