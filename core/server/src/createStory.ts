2d9aeacffb4381ab145f9bd7435b19da67592c42
shellboy <qkrdmstlr3@naver.com>
2022-08-13 01:11:46 +0900

    refactor(server, core): createStory함수 로직 사이드이펙트 제거

--- core/server/src/createStory.ts
+++ core/server/src/createStory.ts
@@ -2,10 +2,18 @@ import { createStoryRequestValidate, CreateStoryResponse } from '@codepocket/sch
 import { PocketToken, StoryInfo, StoryInfoWithCode } from 'types';
 
 export interface CreateStoryType<Response> {
+  /* validator에러 */
   validateErrorFunc?: Response;
+  /* 존재하는 스토리가 있다면 에러 */
+  existStoryErrorFunc: () => Response;
+
+  /* 성공했을 경우 */
   successResponseFunc: (body: CreateStoryResponse) => Response;
+  /* 스토리가 존재하는지 체크하는 함수 */
   isStoryExist: (param: StoryInfo) => Promise<boolean>;
+  /* 토큰으로 유저이름을 가져오는 함수 */
   getUserName: (params: PocketToken) => Promise<string>;
+  /* 스토리 생성 함수 */
   createStory: (params: StoryInfoWithCode) => Promise<void>;
 }
 
@@ -15,7 +23,9 @@ export default async <T, Response>(request: T, modules: CreateStoryType<Response
 
   const storyAuthor = await modules.getUserName({ pocketToken });
 
-  await modules.isStoryExist({ codeAuthor, codeName, storyAuthor, storyName });
+  const isExistStory = await modules.isStoryExist({ codeAuthor, codeName, storyAuthor, storyName });
+  if (isExistStory) return modules.existStoryErrorFunc();
+
   await modules.createStory({ codeAuthor, codeName, storyAuthor, storyName, codes });
 
   return modules.successResponseFunc({ message: '' });
