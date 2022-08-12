a10502b00b5be86cd53b10b8ece292a8d7abf476
shellboy <qkrdmstlr3@naver.com>
2022-08-12 16:00:40 +0900

    refactor(cli): 이름 변경

--- cli/lib/env.ts
+++ cli/lib/env.ts
@@ -1,5 +1,5 @@
 // 유저가 직접 입력
-export const { POCKET_TOKEN } = process.env;
+export const TOKEN = process.env.POCKET_TOKEN;
 
 export const WEB_DEV_URL = process.env.BASE_CLIENT_URL_DEV;
 export const WEB_PROD_URL = process.env.BASE_CLIENT_URL_PROD;
--- cli/lib/utils.ts
+++ cli/lib/utils.ts
@@ -1,7 +1,7 @@
 import { JwtType } from '@codepocket/schema';
 import jwt from 'jsonwebtoken';
 
-import { POCKET_TOKEN } from './env';
+import { TOKEN } from './env';
 
 export const logger = {
   info: console.log,
@@ -9,7 +9,7 @@ export const logger = {
 };
 
 export const getBaseUrl = () => {
-  const token = POCKET_TOKEN;
+  const token = TOKEN;
   if (!token) throw new Error('발급받은 토큰을 환경변수에 넣어주세요!');
 
   const decoded = jwt.verify(token, 'key') as JwtType;
