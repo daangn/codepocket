2e5d58c8ae019d457ecc4bba196fcdaa995e1b5f
shellboy <qkrdmstlr3@naver.com>
2022-08-16 01:19:31 +0900

    chore: 잘못된 환경변수 수정

--- client/src/__mocks__/handlers.ts
+++ client/src/__mocks__/handlers.ts
@@ -1,10 +1,10 @@
 import { VerifyUserResponse } from '@codepocket/schema';
 import { rest } from 'msw';
 
-import { BASE_LOCAL_SERVER_URL } from '../shared/constant';
+import { BASE_SERVER_URL } from '../shared/constant';
 
 export const verifyUserHandler = () =>
-  rest.post(`${BASE_LOCAL_SERVER_URL}/user/auth`, (_, res, ctx) => {
+  rest.post(`${BASE_SERVER_URL}/user/auth`, (_, res, ctx) => {
     return res(
       ctx.status(200),
       ctx.json<VerifyUserResponse>({ validUser: true, userName: 'shell', message: 'success' }),
