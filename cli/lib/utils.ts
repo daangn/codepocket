0316af975f1eac5ac534ba48e16fdeeea7896de3
shellboy <qkrdmstlr3@naver.com>
2022-08-16 16:52:27 +0900

    feat(cli): cli에서 토큰을 가져와 파싱할 때 에러메시지 처리

--- cli/lib/utils.ts
+++ cli/lib/utils.ts
@@ -8,11 +8,16 @@ export const logger = {
   error: console.error,
 };
 
-export const getBaseUrl = () => {
-  if (!TOKEN) throw new Error('발급받은 토큰을 환경변수에 넣어주세요!');
+export const getBaseUrl = (): string => {
+  try {
+    if (!TOKEN) throw new Error('발급받은 토큰을 환경변수에 넣어주세요!');
 
-  // NOTE: KEY값은 server/src/dbModule/user.ts의 KEY변수와 동일하게 맞춰주세요
-  const KEY = 'key';
-  const decoded = jwt.verify(TOKEN, KEY) as JwtType;
-  return decoded.serverUrl;
+    // NOTE: KEY값은 server/src/dbModule/user.ts의 KEY변수와 동일하게 맞춰주세요
+    const KEY = 'key';
+    const decoded = jwt.verify(TOKEN, KEY) as JwtType;
+    return decoded.serverUrl;
+  } catch (error) {
+    logger.error((error as Error).message);
+    return '';
+  }
 };
