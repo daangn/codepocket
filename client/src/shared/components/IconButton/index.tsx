34b469c586c480cc8191b3d6160e16623b08ff82
junghyeonsu <jung660317@naver.com>
2022-08-18 18:05:33 +0900

    feat: IconButton 주석 변경 및 onClick 추가

--- client/src/shared/components/IconButton/index.tsx
+++ client/src/shared/components/IconButton/index.tsx
@@ -4,18 +4,20 @@ import * as style from './style.css';
 
 interface IconButtonInterface {
   icon: ReturnType<typeof Icon>;
+  onClick?: () => void;
 }
 
 /**
- * @description 어떤 알림 뷰를 보여주고 싶을 때 사용해요
- * @example 
- *  <IconButton>
-      <Alert.Title>타이틀이에요</Alert.Title>
-      <Alert.Description>설명이에요</Alert.Description>
-    </IconButton>
+ * @description 아이콘이 들어간 버튼을 만들고 싶을 때 사용해요
+ * @example
+ *  <IconButton icon={<Icon icon="iconName" />} />
  */
-const IconButton = ({ icon }: IconButtonInterface) => {
-  return <button className={style.iconButton}>{icon}</button>;
+const IconButton = ({ icon, onClick }: IconButtonInterface) => {
+  return (
+    <button type="button" onClick={onClick} className={style.iconButton}>
+      {icon}
+    </button>
+  );
 };
 
 export default IconButton;
