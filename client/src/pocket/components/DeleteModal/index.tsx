d94fa2252e75c881a1e10c661d18d203034bb0a0
junghyeonsu <jung660317@naver.com>
2022-08-18 21:35:04 +0900

    chore: toggleModal 함수 메모이제이션

--- client/src/pocket/components/DeleteModal/index.tsx
+++ client/src/pocket/components/DeleteModal/index.tsx
@@ -1,4 +1,5 @@
 import { Modal } from '@shared/components';
+import { useCallback } from 'react';
 
 import { useModalDispatch, useModalState } from '../../contexts/ModalContext';
 import * as style from './style.css';
@@ -7,21 +8,15 @@ const DeleteModal = () => {
   const dispatch = useModalDispatch();
   const { isDeleteModalOpen } = useModalState();
 
+  const toggleModal = useCallback(() => dispatch({ type: 'TOGGLE_DELETE_MODAL' }), [dispatch]);
+
   return (
-    <Modal
-      disableEscape
-      isOpen={isDeleteModalOpen}
-      closeModal={() => dispatch({ type: 'TOGGLE_DELETE_MODAL' })}
-    >
+    <Modal disableEscape isOpen={isDeleteModalOpen} closeModal={toggleModal}>
       <div className={style.deleteModalContainer}>
         <h1 className={style.deleteModalHeaderText}>정말로 삭제하시겠어요?</h1>
         <div className={style.deleteModalButtonContainer}>
-          <Modal.ConfirmButton
-            text="삭제하기"
-            variant="warn"
-            onConfirm={() => dispatch({ type: 'TOGGLE_DELETE_MODAL' })}
-          />
-          <Modal.CancelButton onCancel={() => dispatch({ type: 'TOGGLE_DELETE_MODAL' })} />
+          <Modal.ConfirmButton text="삭제하기" variant="warn" onConfirm={toggleModal} />
+          <Modal.CancelButton onCancel={toggleModal} />
         </div>
       </div>
     </Modal>
--- client/src/pocket/components/EditModal/index.tsx
+++ client/src/pocket/components/EditModal/index.tsx
@@ -1,4 +1,5 @@
 import { Modal } from '@shared/components';
+import { useCallback } from 'react';
 
 import { useModalDispatch, useModalState } from '../../contexts/ModalContext';
 import * as style from './style.css';
@@ -7,20 +8,15 @@ const EditModal = () => {
   const dispatch = useModalDispatch();
   const { isEditModalOpen } = useModalState();
 
+  const toggleModal = useCallback(() => dispatch({ type: 'TOGGLE_EDIT_MODAL' }), [dispatch]);
+
   return (
-    <Modal
-      disableEscape
-      isOpen={isEditModalOpen}
-      closeModal={() => dispatch({ type: 'TOGGLE_EDIT_MODAL' })}
-    >
+    <Modal disableEscape isOpen={isEditModalOpen} closeModal={toggleModal}>
       <div className={style.editModalContainer}>
         <h1 className={style.editModalHeaderText}>수정할 내용을 입력해주세요</h1>
         <div className={style.editModalButtonContainer}>
-          <Modal.ConfirmButton
-            text="수정하기"
-            onConfirm={() => dispatch({ type: 'TOGGLE_EDIT_MODAL' })}
-          />
-          <Modal.CancelButton onCancel={() => dispatch({ type: 'TOGGLE_EDIT_MODAL' })} />
+          <Modal.ConfirmButton text="수정하기" onConfirm={toggleModal} />
+          <Modal.CancelButton onCancel={toggleModal} />
         </div>
       </div>
     </Modal>
