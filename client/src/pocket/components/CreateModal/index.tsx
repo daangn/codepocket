8acc1c31306e692ff21d71697e4c8885911cd66f
shellboy <qkrdmstlr3@naver.com>
2022-08-22 11:53:51 +0900

    feat(client): CreateModal UI 및 기능 구현

--- client/src/pocket/components/CreateModal/index.tsx
+++ client/src/pocket/components/CreateModal/index.tsx
@@ -1,20 +1,96 @@
+import * as Sandpack from '@codesandbox/sandpack-react';
+import { useActiveCode } from '@codesandbox/sandpack-react';
 import { Modal } from '@shared/components';
 import type { ModalInterface } from '@shared/contexts/ModalContext';
+import { localStorage } from '@shared/utils/localStorage';
+import { rem } from 'polished';
+import React, { useState } from 'react';
 
+import useCreateCode from '../../hooks/useCreateCode';
 import * as style from './style.css';
 
-const CreateModal = ({ closeModal, targetId }: ModalInterface) => {
-  const editCode = () => {
-    console.log(targetId);
+interface ModalContentProps {
+  closeModal?: () => void;
+}
+
+const ModalContent = ({ closeModal }: ModalContentProps) => {
+  const { code } = useActiveCode();
+  const [codeName, setCodeName] = useState('');
+  const [useAnonymousMode, setUseAnonymousMode] = useState(true);
+  const { createCode } = useCreateCode();
+
+  const onConfirm = () => {
+    if (!closeModal) return;
+    createCode({
+      code,
+      codeName,
+      isAnonymous: useAnonymousMode,
+      pocketToken: localStorage.getUserToken() || '',
+    });
+    closeModal();
   };
 
+  const onChangeAnonymousToggle = (event: React.ChangeEvent<HTMLInputElement>) =>
+    setUseAnonymousMode(event.target.checked);
+  const onChangeCodeName = (event: React.ChangeEvent<HTMLInputElement>) =>
+    setCodeName(event.target.value);
+
   return (
-    <div className={style.editModalContainer}>
-      <h1 className={style.editModalHeaderText}>수정할 내용을 입력해주세요</h1>
-      <div className={style.editModalButtonContainer}>
-        <Modal.ConfirmButton text="만들기" onConfirm={editCode} />
+    <div className={style.createModalContent}>
+      <h1 className={style.createModalHeaderText}>새로운 코드 등록하기</h1>
+      <div className={style.createModalHeaderWrapper}>
+        <div style={{ flexGrow: 8 }}>
+          <label className={style.label} htmlFor="codename">
+            코드이름
+          </label>
+          <input
+            autoFocus
+            id="codename"
+            value={codeName}
+            className={style.createModalTitleInput}
+            onChange={onChangeCodeName}
+          />
+        </div>
+        <div>
+          <label className={style.label} htmlFor="title-input">
+            익명
+          </label>
+          <label className={style.toggleSwitch}>
+            <input
+              id="title-input"
+              type="checkbox"
+              className={style.toggleSwitchInput}
+              checked={useAnonymousMode}
+              onChange={onChangeAnonymousToggle}
+            />
+            <span className={style.toggleSwitchSlider}></span>
+          </label>
+        </div>
+      </div>
+      <label className={style.label} htmlFor="code">
+        코드
+      </label>
+      <Sandpack.SandpackLayout style={{ height: '100%' }}>
+        <Sandpack.SandpackCodeEditor id="code" style={{ height: '100%', fontSize: rem(18) }} />
+      </Sandpack.SandpackLayout>
+      <div className={style.createModalButtonContainer}>
+        <Modal.ConfirmButton text="만들기" onConfirm={onConfirm} />
         <Modal.CancelButton text="닫기" onCancel={closeModal} />
       </div>
+    </div>
+  );
+};
+
+const CreateModal = ({ closeModal }: ModalInterface) => {
+  return (
+    <div className={style.createModalContainer}>
+      <Sandpack.SandpackProvider
+        template="react-ts"
+        files={{ '/App.tsx': '' }}
+        style={{ height: '100%' }}
+      >
+        <ModalContent closeModal={closeModal} />
+      </Sandpack.SandpackProvider>
     </div>
   );
 };
--- client/src/pocket/components/CreateModal/style.css.ts
+++ client/src/pocket/components/CreateModal/style.css.ts
@@ -1,9 +1,87 @@
+import { vars } from '@seed-design/design-token';
+import * as t from '@shared/styles/token.css';
 import * as u from '@shared/styles/utils.css';
 import { style } from '@vanilla-extract/css';
 import { rem } from 'polished';
 
-export const editModalContainer = style([u.flexColumn, { rowGap: rem(10) }]);
+export const label = style([{ display: 'block', fontSize: rem(18) }]);
 
-export const editModalButtonContainer = style([u.flex, { columnGap: rem(10) }]);
+export const createModalContainer = style([{ width: rem(700), height: '90vh' }]);
 
-export const editModalHeaderText = style({ fontSize: rem(20) });
+export const createModalTitleInput = style([
+  u.border,
+  u.fullWidth,
+  u.borderRadius2,
+  {
+    outline: 'none',
+    height: rem(34),
+    fontSize: rem(16),
+    paddingLeft: rem(15),
+  },
+]);
+
+export const createModalContent = style([
+  u.flexColumn,
+  u.fullHeight,
+  { rowGap: rem(10), justifyContent: 'space-around' },
+]);
+
+export const createModalButtonContainer = style([u.flex, { columnGap: rem(10) }]);
+
+export const createModalHeaderWrapper = style([u.flex, { gap: rem(10) }]);
+
+export const createModalHeaderText = style([t.typography.heading4]);
+
+export const toggleSwitch = style([
+  { position: 'relative', display: 'inline-block', width: rem(60), height: rem(34) },
+]);
+
+export const toggleSwitchInput = style({
+  opacity: 0,
+  width: 0,
+  height: 0,
+});
+
+export const toggleSwitchSlider = style([
+  u.cursorPointer,
+  u.positionAbsolute,
+  {
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: '#ccc',
+    transition: '0.3s',
+    borderRadius: rem(6),
+
+    ':before': {
+      content: '',
+      position: 'absolute',
+      height: rem(26),
+      width: rem(26),
+      left: rem(4),
+      bottom: rem(4),
+      backgroundColor: vars.$static.color.staticWhite,
+      transition: '0.3s',
+      borderRadius: rem(6),
+    },
+
+    selectors: {
+      [`${toggleSwitchInput}:checked + &`]: {
+        backgroundColor: `${vars.$scale.color.blue600}`,
+      },
+
+      [`${toggleSwitchInput}:checked:hover + &`]: {
+        backgroundColor: `${vars.$scale.color.blue400}`,
+      },
+
+      [`${toggleSwitchInput}:checked + &:before`]: {
+        transform: `translateX(26px)`,
+      },
+
+      [`${toggleSwitchInput}:focus + &`]: {
+        boxShadow: `0 0 1px ${vars.$scale.color.blue600}`,
+      },
+    },
+  },
+]);
