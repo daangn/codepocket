859b4c9f843a6bee705ebd56facf925d8ca93952
shellboy <qkrdmstlr3@naver.com>
2022-08-22 18:13:10 +0900

    feat(client): useCustomInfiniteQuery hook 타입 지정

--- client/src/shared/hooks/useCustomInfiniteQuery.ts
+++ client/src/shared/hooks/useCustomInfiniteQuery.ts
@@ -1,35 +1,38 @@
-import { QueryFunctionContext, QueryKey, useInfiniteQuery } from '@tanstack/react-query';
+import { QueryFunctionContext, useInfiniteQuery } from '@tanstack/react-query';
 
 import { APIErrorType, axiosInstance } from '../lib/axios';
 
+type ParamsType = { [param: string]: string | number | undefined };
 interface useCustomInfiniteQueryInterface {
   url: string;
-  params?: { [param: string]: string };
+  params?: ParamsType;
 }
 
-interface Fetcher {
+interface Fetcher<Response> {
+  data: Response;
   nextPage: number;
   prevPage: number;
 }
 
-const fetcher = async <T>({ queryKey, pageParam = 0 }: QueryFunctionContext): Promise<T> => {
+const fetcher = async <T>({
+  queryKey,
+  pageParam = 0,
+}: QueryFunctionContext<[string, ParamsType?]>): Promise<Fetcher<T>> => {
   const [url, params] = queryKey;
-  const { data } = await axiosInstance.get(
-    url as string,
-    { params: { ...(params as any), offset: pageParam } } as any,
-  );
-  return { data, nextPage: pageParam + 1, prevPage: pageParam - 1 } as any;
+  const { data } = await axiosInstance.get(url, { params: { ...params, offset: pageParam } });
+  return { data, nextPage: pageParam + 1, prevPage: pageParam - 1 };
 };
 
 const useCustomInfiniteQuery = <Response>({ url, params }: useCustomInfiniteQueryInterface) => {
-  const res = useInfiniteQuery<Response & Fetcher, APIErrorType, Response, QueryKey>(
-    [url!, params],
-    ({ queryKey, meta, pageParam }) => fetcher({ queryKey, meta, pageParam }),
-    {
-      getNextPageParam: (lastPage) => lastPage.nextPage,
-      getPreviousPageParam: (firstPage) => firstPage.prevPage,
-    },
-  );
+  const res = useInfiniteQuery<
+    Fetcher<Response>,
+    APIErrorType,
+    Fetcher<Response>,
+    [string, ParamsType?]
+  >([url!, params], ({ queryKey, meta, pageParam }) => fetcher({ queryKey, meta, pageParam }), {
+    getNextPageParam: (lastPage) => lastPage.nextPage,
+    getPreviousPageParam: (firstPage) => firstPage.prevPage,
+  });
 
   return res;
 };
